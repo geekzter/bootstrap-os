fb727ff6e8432a4d301311e521f62814368cb898
Eric van Wijk <ericvan@microsoft.com>
2019-11-27 17:45:33 +0000

    Generic bash bootstrap for Linux

---
+++ linux/bootstrap_linux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# curl -sk https://raw.githubusercontent.com/geekzter/bootstrap-os/master/linux/bootstrap_linux.sh | bash
+
+SCRIPTPATH=`dirname $0`
+
+if test ! $(which git); then
+    echo $'\nGit not found, exiting'
+    exit 1
+fi
+
+# Test whether we are part of a cloned repository
+descriptionFile=$(dirname $SCRIPTPATH)/../.git/description
+if [ -f $descriptionFile ]; then
+    if grep -q bootstrap-os "$descriptionFile"; then
+        echo "Repository exists at $(cd $SCRIPTPATH/.. && pwd)"
+        git -C .. pull
+
+        # Done, spawn 2nd stage
+        . ${SCRIPTPATH}/bootstrap_linux2.sh $0
+        exit
+    fi
+fi
+
+echo "Repository does not exist, creating at ~/src/bootstrap-os..."
+if [ ! -d ~/src ]; then
+    mkdir ~/src
+fi
+if [ ! -d ~/src/bootstrap-os ]; then
+    git clone https://github.com/geekzter/bootstrap-os ~/src/bootstrap-os
+fi
+
+. ${SCRIPTPATH}/bootstrap_linux2.sh $0
---
+++ linux/bootstrap_linux2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH=`dirname $0`
+
+# Packages
+if test ! $(which apt-get); then
+    echo $'\napt-get not found, skipping packages'
+else
+    if test ! $(which apt-get); then
+        echo $'\nsudo not found, skipping packages'
+    else
+        echo $'\nInstalling/updating packages...'
+        xargs -a ${SCRIPTPATH}/apt-packages.txt sudo apt-get install -y
+    fi
+fi
+
+# PowerShell
+if test ! $(which git); then
+    echo $'\nPowerShell Core (pwsh) not found, skipping setup'
+else
+    echo $'\nSetting up PowerShell Core..'
+    pwsh -nop -file $SCRIPTPATH/../common/bootstrap_pwsh.ps1
+fi
+
+# Set up terraform with tfenv
+### Check if a directory does not exist ###
+if [ ! -d ~/.tfenv ]; then
+    echo $'\nInstalling tfenv...'
+    git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+else
+    echo $'\nUpdating tfenv...'
+    git -C ~/.tfenv pull
+fi
+~/.tfenv/bin/tfenv install latest
+
+# Git settings
+if [ -f ../common/settings.json ]; then
+    git config --global user.email $(cat ../common/settings.json | jq '.GitEmail')
+    git config --global user.name "$(cat ../common/settings.json | jq '.GitName')"
+else
+    echo $'\n'
+    echo "Settings file $(cd $SCRIPTPATH/../common/ && pwd)/settings.json not found, skipping personalization"
+fi
