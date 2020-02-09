d20897d991f653a3451357729c255f22b096f7d5
Eric van Wijk <eric@van-wijk.com>
2020-02-09 17:51:38 +0100

    Disable IE security for users too

--- windows/bootstrap_windows.ps1
+++ windows/bootstrap_windows.ps1
@@ -43,6 +43,11 @@ if ($ieSecMode -and ($ieSecMode -ne 0)) {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 
     Stop-Process -Name Explorer -Force # Should spawn a new process
 }
+$ieSecMode = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty IsInstalled
+if ($ieSecMode -and ($ieSecMode -ne 0)) {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 
+    Stop-Process -Name Explorer -Force # Should spawn a new process
+}
 
 # Install Chocolatey
 if (!(Get-Command "choco.exe" -ErrorAction SilentlyContinue)) {
