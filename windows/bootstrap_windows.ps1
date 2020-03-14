b89188dd2f4db731571cb4a2b62807cb1fc38b28
Eric van Wijk <eric@van-wijk.com>
2020-03-14 15:26:55 +0100

    .

--- windows/bootstrap_windows.ps1
+++ windows/bootstrap_windows.ps1
@@ -39,6 +39,10 @@ if (!(New-Object System.Security.Principal.WindowsPrincipal([System.Security.Pri
 
 # Disable IE Enhanced Security Mode
 $ieSecMode = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty IsInstalled
+if ($ieSecMode -and ($ieSecMode -ne 0)) {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 
+    Stop-Process -Name Explorer -Force -ErrorAction SilentlyContinue # Should spawn a new process
+}
 $ieSecMode = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty IsInstalled
 if ($ieSecMode -and ($ieSecMode -ne 0)) {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 
