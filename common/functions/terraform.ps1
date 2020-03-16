e7cec3afed87798d8fb566d17fab4957fb517bd1
Eric van Wijk <ericvan@microsoft.com>
2020-03-16 14:28:40 +0000

    Additional Terraform functions

--- common/functions/terraform.ps1
+++ common/functions/terraform.ps1
@@ -1,5 +1,5 @@
 function Apply-Terraform {
-    terraform apply -auto-approve
+    Invoke "terraform apply -auto-approve"
 }
 Set-Alias tfa Apply-Terraform
 
@@ -12,12 +12,13 @@ function CD-Terraform {
     } else {
         Write-Warning "Terraform directory not found"
     }
+
+    return $main
 }
 Set-Alias cdtf CD-Terraform
 Set-Alias tfcd CD-Terraform
 
 function Clear-TerraformState {
-    #terraform state list | xargs -L 1 terraform state rm
     terraform state list | ForEach-Object { 
         terraform state rm $_
     }
@@ -25,11 +26,50 @@ function Clear-TerraformState {
 Set-Alias tfclr Clear-TerraformState
 
 function Destroy-Terraform {
-    terraform destroy # -auto-approve
+    Invoke "terraform destroy" # -auto-approve
 }
 Set-Alias tfd Destroy-Terraform
 
+function Get-TerraformInfo {
+    if (Get-Command git -ErrorAction SilentlyContinue) {
+        Invoke-Command -ScriptBlock {
+            $Private:ErrorActionPreference = "SilentlyContinue"
+            $branch = $(git rev-parse --abbrev-ref HEAD 2>$null)
+            if ($branch) {
+                Write-Host "Git branch: $branch"
+            }
+        }
+    }
+    Write-Host "Terraform workspace: $(terraform workspace show)"
+    Get-ChildItem -Path Env: -Recurse -Include ARM_*,TF_* | Sort-Object -Property Name
+}
+Set-Alias tfi Get-TerraformInfo
+
+function Invoke (
+    [string]$cmd
+) {
+    Write-Host "`n$cmd" -ForegroundColor Green 
+    Invoke-Expression $cmd
+    if ($LASTEXITCODE -ne 0) {
+        exit
+    }
+}
+
+function Invoke-Terraform (
+    [string]$cmd
+) {
+    $main = CD-Terraform
+    Write-Host "`n$cmd" -ForegroundColor Green 
+    Invoke-Expression $cmd
+    if ($LASTEXITCODE -ne 0) {
+        exit
+    }
+    if ($main) {
+        Pop-Location
+    }
+}
+
 function List-TerraformState {
-    terraform state list
+    Invoke "terraform state list"
 }
 Set-Alias tfls List-TerraformState
