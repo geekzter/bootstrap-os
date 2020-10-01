57572589894e7d9208bc0a4d1de98b4d568ea6c8
Eric van Wijk <eric@van-wijk.com>
2020-10-01 20:40:09 +0200

    Streamline for blog post

--- common/functions/remoteaccess.ps1
+++ common/functions/remoteaccess.ps1
@@ -1,32 +1,37 @@
+#Requires -Version 7.0
 function Connect-TmuxSession (
-    [string]$Workspace=($env:TF_WORKSPACE ? $env:TF_WORKSPACE : "default")
+    # Use Terraform workspace variable as default session name
+    [string]$Workspace=($env:TF_WORKSPACE ? $env:TF_WORKSPACE : "default") 
 ) {
     if ($env:TMUX) {
         Write-Warning "Session already exixts: $env:TMUX"
         return
     }
 
+    if (!(Get-Command tmux -ErrorAction SilentlyContinue)) {
+        Write-Warning "tmux not found"
+        return
+    }
+
     if (!$Workspace -and (Get-Command terraform)) {
+        # Getworkspace name from current Terraform workspace, if installed
         $Workspace = $(terraform workspace show 2>$null)
     }
 
     # Set locale as it may be missing and is required for tmux
-    if (!$env:LANG) {
-        $env:LANG="en_US.UTF-8"
-    }
-    if (!$env:LC_ALL) {
-        $env:LC_ALL=$env:LANG
-    }
+    $env:LANG   ??= "en_US.UTF-8"
+    $env:LC_ALL ??= $env:LANG
 
     $prexistingSession = $(tmux ls -F "#S" 2>$null) -match "^${Workspace}$"
     if (!$prexistingSession) {
         # Start session, but do not yet attach
         tmux new -d -s $Workspace
+        # Set Terraform workspace to the name of the session
         Write-Verbose "`$env:TF_WORKSPACE='$Workspace'"
         tmux send-keys -t $Workspace "`$env:TF_WORKSPACE='$Workspace'" Enter
     }
 
-    # Attach
+    # Attach to session
     tmux attach-session -d -t $Workspace
 
 }
