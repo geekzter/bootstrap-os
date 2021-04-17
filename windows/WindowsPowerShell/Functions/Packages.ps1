18f395d170880b21df5023d41c51059c7eff037e
Eric van Wijk <eric@van-wijk.com>
2021-04-17 17:01:48 +0200

    Reset Terraform to 0.14.10

---
+++ windows/WindowsPowerShell/Functions/Packages.ps1
@@ -0,0 +1,10 @@
+function Install-Terraform (
+    [parameter(Mandatory=$false)][string]$Version="0.14.10"
+) {
+    if (get-command choco -ErrorAction SilentlyContinue) {
+        choco upgrade terraform --version $Version --allow-downgrade -y
+    } else {
+        Write-Warning "This depends on Chocolatey, which was not found"
+        return
+    }
+}
