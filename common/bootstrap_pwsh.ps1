275fdcbc92d93477afcf2fc30542c5f60f854d52
Eric van Wijk <eric@van-wijk.com>
2022-02-03 08:40:48 +0100

    Re-enable PSReadLine

--- common/bootstrap_pwsh.ps1
+++ common/bootstrap_pwsh.ps1
@@ -23,7 +23,7 @@ if (-not $NoPackages) {
         # The PSDesiredStateConfiguration module was removed from the PowerShell 7.2 package and is now published to the PowerShell Gallery
         AddorUpdateModule PSDesiredStateConfiguration
     }
-    # AddorUpdateModule PSReadLine
+    AddorUpdateModule PSReadLine
     # https://docs.microsoft.com/en-us/powershell/scripting/whats-new/what-s-new-in-powershell-72?view=powershell-7.2#psreadline-21-predictive-intellisense
     $psReadLineOptionCommand = (Get-Command Set-PSReadLineOption -ErrorAction SilentlyContinue)
     if ($psReadLineOptionCommand -and $psReadLineOptionCommand.Version -ge 2.1) {
