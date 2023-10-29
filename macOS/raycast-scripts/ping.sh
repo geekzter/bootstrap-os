64150c4166f0acbe064565f85487b5e90225c48d
Eric van Wijk <eric@van-wijk.com>
2023-10-29 14:19:30 +0100

    ping

---
+++ macOS/raycast-scripts/ping.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Ping connectivity
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon images/terminal-icon.icns
+# @raycast.needsConfirmation false
+
+# Documentation:
+# @raycast.description Ping Google DNS server
+# @raycast.author Eric van Wijk
+# @raycast.authorURL https://github.com/ericvan
+
+ping 8.8.8.8
